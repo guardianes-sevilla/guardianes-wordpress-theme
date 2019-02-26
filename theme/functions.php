d22ebc1982431c0d53ea62c21cf516666b8de819
luki <lukimartinezbayon@gmail.com>
2019-02-26 16:05:11 +0100

    add widgets

--- theme/functions.php
+++ theme/functions.php
@@ -13,3 +13,22 @@ function mis_menus() {
   );
 }
 add_action( 'init', 'mis_menus' );
+
+/**
+ * Crear una zonan de widgets que podremos gestionar
+ * fácilmente desde administrador de Wordpress.
+ */
+
+function mis_widgets(){
+ register_sidebar(
+   array(
+       'name'          => __( 'Sidebar' ),
+       'id'            => 'sidebar',
+       'before_widget' => '<div class="widget">',
+       'after_widget'  => '</div>',
+       'before_title'  => '<h3>',
+       'after_title'   => '</h3>',
+   )
+ );
+}
+add_action('init','mis_widgets');
