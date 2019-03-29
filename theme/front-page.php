3309f84675ae8544f963a275aea13b74ebfe233a
Juan D. Jara <juanorigami@gmail.com>
2019-03-29 10:15:14 +0100

    styles for social links

--- theme/front-page.php
+++ theme/front-page.php
@@ -4,8 +4,11 @@
   <img class="logo animated bounceInRight slow" src="/wp-content/uploads/2019/03/Escudo-Flat-blanco-01-1.png" />
   <section class="menus animated bounceInLeft slow">
     <h1>Asociación Guardianes</h1>
-    <?php wp_nav_menu( array( 'theme_location' => 'navegation', 'menu_class' => 'menu animated bounceInLeft delay-1s' ) ); ?>
-    <?php wp_nav_menu( array( 'theme_location' => 'social', 'menu_class' => 'menu animated bounceInLeft delay-1s' ) ); ?>
+    <div class="animated bounceInLeft delay-1s">
+      <?php wp_nav_menu( array( 'theme_location' => 'social', 'menu_class' => 'menu social' ) ); ?>
+      <?php wp_nav_menu( array( 'theme_location' => 'navegation', 'menu_class' => 'menu primary' ) ); ?>
+      <p class="join-aso">¿Quieres unirte a Guardianes? <a href="">Pulsa aquí</a> </p>
+    </div>
   </section>
 </main>
 <!-- Archivo de pié global de Wordpress -->
